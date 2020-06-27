ac4ebc445e87e905b147a47618242f32d8c5587f
andead422 <andy.kuleshov@gmail.com>
2020-06-27 16:26:30 +0300

    2020/06/27 16:26

--- bot_code.py
+++ bot_code.py
@@ -449,7 +449,8 @@ def queue(bot, message):
             cur.execute(f"SELECT user_id FROM identify WHERE user_id = '{user_id}'")
             user_id_list = cur.fetchall()
             if len(user_id_list) == 1:
-                cur.execute(f"UPDATE stat SET name = '{name_in_sql}', surname = '{surname_in_sql}', qty = qty + 1 WHERE user_id = '{user_id}'")
+                cur.execute(f"UPDATE stat SET qty = qty + 1 WHERE user_id = '{user_id}'")
+                cur.execute(f"UPDATE identify SET name = '{name_in_sql}', surname = '{surname_in_sql}' WHERE user_id = '{user_id}'")
             elif len(user_id_list) == 0:
                 cur.execute(f"INSERT INTO identify VALUES ('{name_in_sql}', '{surname_in_sql}', '{user_id}')")
                 cur.execute(f"INSERT INTO stat (qty, user_id) VALUES (1, '{user_id}')")
